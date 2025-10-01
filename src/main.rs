bccba657e85fc887cee4ddc22acd9a1c34d53734
Sebastian Wiesner <sebastian@swsnr.de>
2025-10-01 22:12:56 +0200

    cargo new + Cargo.toml metadata

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+// Copyright Sebastian Wiesner <sebastian@swsnr.de>
+//
+// Licensed under the EUPL-1.2 OR GPL-3.0
+//
+// See https://interoperable-europe.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+
+fn main() {
+    println!("Hello, world!");
+}
