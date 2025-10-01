306784b541bc8318a1e9b41c6496048bda98ee08
Sebastian Wiesner <sebastian@swsnr.de>
2025-10-01 22:13:50 +0200

    Tighten lint and compiler rules

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,33 @@
 //
 // See https://interoperable-europe.ec.europa.eu/collection/eupl/eupl-text-eupl-12
 
+#![deny(warnings, clippy::all, clippy::pedantic,
+    // Do cfg(test) right
+    clippy::cfg_not_test,
+    clippy::tests_outside_test_module,
+    // Guard against left-over debugging output
+    clippy::dbg_macro,
+    clippy::unimplemented,
+    clippy::use_debug,
+    clippy::todo,
+    // Don't panic carelessly
+    clippy::get_unwrap,
+    clippy::unused_result_ok,
+    clippy::unwrap_in_result,
+    clippy::indexing_slicing,
+    // Do not carelessly ignore errors
+    clippy::let_underscore_must_use,
+    clippy::let_underscore_untyped,
+    // Code smells
+    clippy::float_cmp_const,
+    clippy::string_to_string,
+    clippy::if_then_some_else_none,
+    clippy::large_include_file,
+    // Disable as casts
+    clippy::as_conversions,
+)]
+#![forbid(unsafe_code)]
+
 fn main() {
     println!("Hello, world!");
 }
